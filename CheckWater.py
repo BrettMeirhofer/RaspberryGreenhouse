9ad9948c388363c2bc6b8114e5186ca25dcd4d5d
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-17 20:57:43 -0600

    Added Water Level web reporting

--- CheckWater.py
+++ CheckWater.py
@@ -2,23 +2,22 @@ import RPi.GPIO as GPIO
 from hcsr04sensor import sensor
 import GreenhouseFuncs
 import datetime
+from SendData import send_sensor_data
 
 
 def record_water():
     trig = 4
     echo = 17
-
-    data_file, writer = GreenhouseFuncs.get_data_file("Water")
-
+    reservoir_height = 30.48
+    sensor_height = 17.78
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    x = sensor.Measurement(trig, echo)
-    distance_warm = x.raw_distance()
-    distance_warm = round(distance_warm * 0.393701, 2)
-    current_time = datetime.datetime.now().strftime("%H%M")
-    writer.writerow([current_time, distance_warm])
-    data_file.close()
+    measure_object = sensor.Measurement(trig, echo)
+    distance_warm = measure_object.raw_distance()
+    water_percent = round((distance_warm - sensor_height) / reservoir_height)
     GPIO.cleanup((trig, echo))
+    json_data = {"date": datetime.datetime.now().strftime("%Y%m%d%H%M"), "water_level": water_percent}
+    send_sensor_data(json_data, "/admin/Water/")
 
 
 if __name__ == '__main__':
--- SendData.py
+++ SendData.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import datetime
 import os
 
 
