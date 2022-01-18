a9f5fd563a2f66153829ed37760a161e64f6179a
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-17 19:36:58 -0600

    Added automatic watering utility
    - Properly closed config file in send_sensor_data

--- SendData.py
+++ SendData.py
@@ -10,4 +10,5 @@ def send_sensor_data(json_data, endpoint):
     target_url = config_dict["website_url"] + endpoint
     headers = {"Authorization": config_dict["website_token"]}
     r = requests.post(target_url, data=json.dumps(json_data), headers=headers, allow_redirects=False)
+    config_file.close()
 
