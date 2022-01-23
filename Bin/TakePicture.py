ca6626274bff682b492d702804eaf8ff1d863eb2
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-22 19:01:42 -0600

    Pictures are now stored in the images directory

--- Bin/TakePicture.py
+++ Bin/TakePicture.py
@@ -4,7 +4,8 @@ import datetime
 
 def take_picture():
     current_time = datetime.datetime.now().strftime("%Y%m%d%H%M")
-    command = "ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 {}.jpg".format(current_time)
+    target_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Images", current_time + ".png")
+    command = "ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 " + target_path
     os.system(command)
 
 
