14f969054d790df5d99f7ac8d6d5f737401bfd3b
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-23 11:55:48 -0600

    Corrected send image data endpoint

--- Bin/SendData.py
+++ Bin/SendData.py
@@ -12,7 +12,7 @@ def send_sensor_data(json_data, endpoint):
 
 def send_image_data(file):
     config_dict = GHF.open_config_dict("WebConfig.json")
-    target_url = config_dict["website_url"] + "/upload_image/"
+    target_url = config_dict["website_url"] + "/admin/upload_image/"
     headers = {"Authorization": config_dict["website_token"]}
     files = {"file_uploaded": file}
     r = requests.post(target_url, files=files, headers=headers, allow_redirects=False)
