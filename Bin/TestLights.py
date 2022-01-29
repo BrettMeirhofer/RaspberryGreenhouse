55637a13306a710e26c9938c4847823dd6d61f2b
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-28 20:40:11 -0600

    Update TestLights.py

--- Bin/TestLights.py
+++ Bin/TestLights.py
@@ -6,7 +6,7 @@ import signal
 import sys
 # format- "device-name": "device-address"
 devices = {
-    "H6008_1996": "9C:04:A0:93:FC:08"
+    "H6008_1996": "9C:04:A0:95:19:96"
 }
 
 device_names = list(devices.keys())
