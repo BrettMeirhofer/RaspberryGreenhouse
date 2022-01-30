0842387b9ecb07ad92bf917062c8d7f24c3e54c2
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-29 19:06:51 -0600

    Added utility for manually controlling the relay remotely

---
+++ Bin/ManualRelay.py
@@ -0,0 +1,9 @@
+import time
+import GreenhouseFuncs as GHF
+import sys
+
+
+# Allows for manual deployment of relay commands without using the browser
+if __name__ == '__main__':
+    logger = GHF.create_logger("ManualRelay")
+    GHF.toggle_relay(sys.argv[1], sys.argv[2])
