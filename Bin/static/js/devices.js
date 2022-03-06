e2772792faa70809b28e9eb805f6254a50acdbba
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-03-05 20:05:12 -0600

    All devices with light in the name are given colors

--- Bin/static/js/devices.js
+++ Bin/static/js/devices.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
+    $.ajaxSetup({
+        async: false
+    });
 
+    var colors
+    $.ajax({
+        url: "/colors",
+        success: function (data) {
+            data = JSON.parse(data)
+            colors = data.colors
+            console.log(colors)
+        }
+    })
+    console.log(colors)
 
-
+    $.ajaxSetup({
+    async: true
+    });
 
     $.ajax({
         url: "/toggles",
@@ -19,34 +34,26 @@ $(document).ready(function () {
                 div.appendChild(create_form("OFF", 0, item))
                 div.appendChild(my_form)
                 $("#toggles").append(div)
-            });
-            var div1=document.createElement('div');
-            $("#light1").append(div1)
-            //Colors
-            $.ajax({
-                url: "/colors",
-                success: function (data) {
-                    data = JSON.parse(data)
-                    data.colors.forEach(function (item, index) {
-                         my_form=document.createElement('FORM');
-                         my_form.method ='POST'
-                         my_form.action = '/control?device=light1' + '&color=' + item
-                         my_tb=document.createElement('BUTTON')
-                         my_tb.classList.add('button')
+                if (item.includes("light")){
+                    var div1=document.createElement('div');
+                    div.append(div1)
+                    colors.forEach(function (item, index) {
+                        my_form=document.createElement('FORM');
+                        my_form.method ='POST'
+                        my_form.action = '/control?device=light1' + '&color=' + item
+                        my_tb=document.createElement('BUTTON')
+                        my_tb.classList.add('button')
                          my_tb.classList.add('button2')
                          console.log("#" + item)
                          my_tb.style.backgroundColor = "#" + item
                          my_form.appendChild(my_tb);
                          div1.appendChild(my_form)
                          console.log(div1)
-            });
+                    })
+                }
+            })
         }
     })
-        }
-    })
-
-
-
 })
 
 
