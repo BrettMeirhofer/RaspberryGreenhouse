89d22bdcff363abf6f28803e75a6b9e902021f18
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-03-13 20:05:29 -0500

    Added support for multiple cameras defined in Config

--- Bin/TakePicture.py
+++ Bin/TakePicture.py
@@ -12,13 +12,16 @@ def take_picture():
     logger = GHF.create_logger("TakePicture")
     current_time = datetime.datetime.now(tz=pytz.UTC).strftime("%Y%m%d%H%M")
     target_path = ""
-    for x in range(3):
-        target_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Images", current_time + str(x) + ".png")
-        command = "ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 " + target_path
-        os.system(command)
+    config_dict = GHF.open_config_dict("Config.json")
+    for camera in config_dict["cams"]:
+        for x in range(3):
+            file_name = camera["name"] + current_time + str(x) + ".png"
+            target_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Images", file_name)
+            command = "ffmpeg -f v4l2 -video_size 1280x720 -i {} -frames 1 {}".format(camera["sys"], target_path)
+            os.system(command)
+            time.sleep(1)
         time.sleep(1)
-    time.sleep(1)
-    SendData.send_image_data(open(target_path, "rb"))
+        SendData.send_image_data(open(target_path, "rb"))
 
 
 if __name__ == '__main__':
