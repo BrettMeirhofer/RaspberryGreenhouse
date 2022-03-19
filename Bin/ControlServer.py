08e2a4680b24304e2b3f709adb788b10bc5cc158
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-03-19 11:52:47 -0500

    Removed failed streaming functionality

--- Bin/ControlServer.py
+++ Bin/ControlServer.py
@@ -51,25 +51,9 @@ def get_colors():
     return json.dumps({"colors": config_dict["colors"]})
 
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-class Camera(object):
-    def __init__(self):
-        self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
-
-    def get_frame(self):
-        return self.frames[int(time()) % 3]
 
 
 GHF.create_logger("ControlServer")
