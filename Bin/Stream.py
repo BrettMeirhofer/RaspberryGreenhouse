ffd3cce94e41fd1d5493836eca7bc6191bf9fee9
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-03-21 17:17:46 -0500

    Changed jpg to img

--- Bin/Stream.py
+++ Bin/Stream.py
@@ -13,7 +13,7 @@ def take_picture():
     config_dict = GHF.open_config_dict("Config.json")
     for camera in config_dict["cams"]:
         file_name = camera["name"] + ".png"
-        target_path = os.path.join(os.path.dirname(__file__), "static", "jpg", file_name)
+        target_path = os.path.join(os.path.dirname(__file__), "static", "img", file_name)
         command = "ffmpeg -f v4l2 -video_size 1280x720 -i {} -frames 1 {}".format(camera["sys"], target_path)
         os.system(command)
         time.sleep(.1)
