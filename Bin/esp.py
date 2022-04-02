3a5ee76b4322548a5e61714442702c7a93d5fd6f
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-04-01 19:53:02 -0500

    Update esp.py

--- Bin/esp.py
+++ Bin/esp.py
@@ -50,7 +50,7 @@ import pygatt
 
 # The BGAPI backend will attempt to auto-discover the serial device name of the
 # attached BGAPI-compatible USB adapter.
-adapter = pygatt.BGAPIBackend()
+adapter = pygatt.GATTToolBackend()
 
 try:
     adapter.start()
