bafb83e46f3d7251e5947e1fc401fd2cc254f39f
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-04-10 00:33:14 -0500

    Update SendData.py

--- Bin/helper/SendData.py
+++ Bin/helper/SendData.py
@@ -6,7 +6,7 @@ import helper.GreenhouseFuncs as GHF
 def send_sensor_data(json_data, endpoint):
     config_dict = GHF.open_config_dict("WebConfig.json")
     target_url = config_dict["website_url"] + endpoint
-    headers = {"Authorization": config_dict["website_token"]}
+    headers = {"Authorization": "Token " + config_dict["website_token"]}
     r = requests.post(target_url, data=json.dumps(json_data), headers=headers, allow_redirects=False)
 
 
