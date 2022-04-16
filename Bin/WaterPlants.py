6280d33901f90b614a7c4a6070219d959dff477b
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-04-16 10:10:28 -0500

    Added config toggle for watering

--- Bin/WaterPlants.py
+++ Bin/WaterPlants.py
@@ -6,11 +6,12 @@ from helper import GreenhouseFuncs as GHF
 def enable_pump():
     logger = GHF.create_logger("WaterPlants")
     config_dict = GHF.open_config_dict("Config.json")
-    pump_relay = config_dict["pump_relay"]
-    GHF.toggle_device(pump_relay, True)
-    time.sleep(config_dict["watering_duration"])
-    GHF.toggle_device(pump_relay, False)
-    logger.info("Successfully watered plants")
+    if config_dict["watering"]:
+        pump_relay = config_dict["pump_relay"]
+        GHF.toggle_device(pump_relay, True)
+        time.sleep(config_dict["watering_duration"])
+        GHF.toggle_device(pump_relay, False)
+        logger.info("Successfully watered plants")
 
 
 if __name__ == '__main__':
